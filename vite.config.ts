b2e9537f0dadf1630e814be4a47b7569c6318d9c
uniboxx <unibox@duck.com>
2024-05-25 02:25:03 +0200

    removed comments from vite.config.js

--- vite.config.ts
+++ vite.config.ts
@@ -2,24 +2,24 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/github-test/',
-  // root: 'src',
-  // server: {
-  //   port: 3000,
-  // },
-  // preview: {
-  //   port: 8080,
-  // },
-  // build: {
-  //   minify: true,
-  //   assetsInlineLimit: 0,
-  //   outDir: '../dist',
-  //   rollupOptions: {
-  //     output: {
-  //       entryFileNames: 'assets/[name].js',
-  //       chunkFileNames: 'assets/[name].js',
-  //       assetFileNames: 'assets/[name].[ext]',
-  //     },
-  //   },
-  // },
+  root: 'src',
+  server: {
+    port: 3000,
+  },
+  preview: {
+    port: 8080,
+  },
+  build: {
+    minify: true,
+    assetsInlineLimit: 0,
+    outDir: '../dist',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]',
+      },
+    },
+  },
   plugins: [],
 });
